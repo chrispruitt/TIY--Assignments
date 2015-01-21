2d0aa2779cee0607423a5e0c0aabb5266725018e
Adam Karbiener <adamkarb@gmail.com>
2015-01-21 15:58:15 -0500

    Finished creating my miniature calculator

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+
+function add(a, b) {
+    return a + b;   
+};
+
+function diff(a, b) {
+    return a - b;   
+};
+
+
+function prod(a, b) {
+    return a * b;   
+};
+
+
+function div(a, b) {
+    return a / b;   
+};
+
+
+var = 
+
+
+
+
