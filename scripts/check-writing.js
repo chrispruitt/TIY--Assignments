45e22bd3c5b349ac2b51b0d7f599e90e8a4ad9ff
Adam Karbiener <adamkarb@gmail.com>
2015-01-25 16:21:12 -0500

    small changes to check writing

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -1,6 +1,25 @@
 var assert = require('assert');
 
-var toEnglish = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+var toEnglish = ['zero', 'one', 'two', 'three', 'four', 'five'];
 
-
+function giveNumber(a) {
+    if (a == 0) {
+        return toEnglish[0];   
+    };
+    else if (a == 1) {
+        return toEnglish[1];   
+    };
+    else if (a == 2) {
+        return toEnglish[2];   
+    };
+    else if (a == 3) {
+        return toEnglish[3];   
+    };
+    else if (a == 4) {
+        return toEnglish[4];   
+    };
+    else if (a == 5) {
+        return toEnglish[5];   
+    };
+}
 
