be0e231ba9939682373d3095f139f0545957f770
Adam Karbiener <adamkarb@gmail.com>
2015-01-27 13:52:52 -0500

    Modified the gym sheet

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -6,8 +6,20 @@ var events = require('../apis/github/users/adamkarb/events.json');
 assert(events.length);
 
 function answer(){
+    var index = 0
+    var event = events[0];
+    var numberOfPushEvents= 0
+    
+    if (firstEvent.type == "PushEvent" ) {
+        numberOfPushEvents++;
+    }
+    
     return {
-        'total': events.length   
+        'total': events.length,
+        'PushEvent': {
+            "total": 0
+        
+        }
     }
 }  //END answer
 
