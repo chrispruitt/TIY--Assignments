b85395b5c3b6b92ea8be8813cd2eb86ca305c2fa
Adam Karbiener <adamkarb@gmail.com>
2015-01-27 10:13:22 -0500

    fixed api gym

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -1,14 +1,41 @@
+var assert = require('assert');
 var events = require('../apis/github/users/adamkarb/events.json');
 
 
 
-//function answer() {
-//    return    
-//}
+assert(events.length);
 
-console.log([2]);
+function answer(){
+    return {
+        'total': events.length   
+    }
+}  //END answer
 
 
+describe('the setup', function(){
+    it('should have events', function(){
+        assert(events);   
+    });
+});
+
+it('should have an `answer` function', function() {
+    assert(answer);
+    assert(typeof answer == 'function');
+});
+
+describe('theanswer', function() {
+    var theAnswer = answer();
+    
+    it('should have 30 total events', function(){
+            assert(theAnswer.total === 30);
+    });
+    
+    it('should have some `PushEvent` entries', function() {
+        assert(theAnswer.PushEvents.total);
+    });
+    
+    
+});
 
 
 //Dont even know how to start
