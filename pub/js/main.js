2a7ceaca7e3b9fa93790b7538b19dac4140fa7dc
Adam Karbiener <adamkarb@gmail.com>
2015-02-06 17:40:31 -0500

    I hate my life... nothing works

--- pub/js/main.js
+++ pub/js/main.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
     
     
     
+    $("button").on('click', function {
+        $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/252/comment?access_token=8acb8c2be6e24b7a0bda74f57f1225c76bed52e8", JSON.stringify({"body": $("#comments").val()})
+                    //$("#comments").val("")
+              );
+    
+    })
+    
     
     
     
@@ -9,3 +16,6 @@ $(document).ready(function() {
     
     
 });
+
+
+$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/252/comments?access_token=8acb8c2be6e24b7a0bda74f57f1225c76bed52e8", JSON.stringify({"body": $("#comments").val()}));
