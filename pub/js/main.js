6da0145c543d1a3638cbe659bf401c5a5a3db361
Adam Karbiener <adamkarb@gmail.com>
2015-02-07 12:26:24 -0500

    Please lord let this button work

--- pub/js/main.js
+++ pub/js/main.js
@@ -18,4 +18,4 @@ $(document).ready(function() {
 });
 
 
-$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/252/comments?access_token=8acb8c2be6e24b7a0bda74f57f1225c76bed52e8", JSON.stringify({"body": $("#comments").val()}));
+//$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/252/comments?access_token=8acb8c2be6e24b7a0bda74f57f1225c76bed52e8", JSON.stringify({"body": $("#comments").val()}));
